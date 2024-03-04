62a29ac4381f07bc66e2be40c27543d229bd2312
Esaie0Guerson <laraesaieleg1@gmail.com>
2024-03-03 19:45:58 -0500

    SingleResponsability

---
+++ Single-responsibility/singleResponsability.php
@@ -0,0 +1,31 @@
+<?php
+
+
+//cela viole le principe de responsabilité unique car la classe a plus d'une raison de changer. 
+class Utilisateur
+{
+private $nom;
+private $email;
+
+    public function __construct($nom, $email)
+    {
+        $this->nom = $nom;
+        $this->email = $email;
+    }
+    public function getNom(){
+ 		return $this->nom;}
+    public function getEmail(){
+       		 return $this->email;}
+
+   	 public function enregistrerDansLaBaseDeDonnees(){
+        // Code pour enregistrer les données de l'utilisateur dans la base de données
+   	 }
+
+   	 public function envoyerNotificationParEmail($message){
+        // Code pour envoyer une notification par e-mail à l'utilisateur
+        // Cette méthode enregistre également le statut d'envoi de la notification dans la base de données
+ 	   }
+}
+
+
+?>
---
+++ Single-responsibility/singleResponsabilityUnique.php
@@ -0,0 +1,35 @@
+<?php
+
+//respecter le principe de responsabilité unique, nous pouvons séparer ces responsabilités en deux classes différentes :
+class Utilisateur
+{
+private $nom;
+private $email;
+
+    public function __construct($nom, $email)
+    {
+        $this->nom = $nom;
+        $this->email = $email;
+    }
+    public function getNom(){
+ 		return $this->nom;}
+    public function getEmail(){
+       		 return $this->email;}
+
+public function enregistrerDansLaBaseDeDonnees(){
+       		 // Code pour enregistrer les données de l'utilisateur dans la base de données
+   	 }
+}
+
+
+
+class ServiceNotificationParEmail
+{
+    public function envoyerNotificationParEmail(Utilisateur $utilisateur, $message) {
+       		// Code pour envoyer une notification par e-mail à l'utilisateur
+              // Cette méthode enregistre également le statut d'envoi de la notification dans la base de données
+                        }
+}
+
+
+?>
